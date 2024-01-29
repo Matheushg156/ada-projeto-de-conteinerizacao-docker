11fdc70c5aa992e1da8cfd1443d56f20ca7f6fd4
Matheushg156 <matheushgrdc156@gmail.com>
2024-01-28 22:03:13 -0300

    Adiciona estilo ao front

--- app/back/src/app.ts
+++ app/back/src/app.ts
@@ -2,8 +2,12 @@ import express from 'express';
 
 import animeListRouter from './routes/animeListRoute';
 
+import cors from 'cors';
+
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
--- app/front/src/App.js
+++ app/front/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
+import AnimeList from './components/AnimeList';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 div className="anime-title">My Anime List</h1>
+      <AnimeList />
     </div>
   );
-}
+};
 
 export default App;
